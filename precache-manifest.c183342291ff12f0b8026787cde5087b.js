47669cbf79bb11aabb52b79ffe0479d1ae8bedd0
MiKoKappa <mikopar@gmail.com>
2020-10-05 09:07:00 +0200

    Updates

---
+++ precache-manifest.c183342291ff12f0b8026787cde5087b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8230a4e7a4717a0463e0f4805b9226ca",
+    "url": "/react-components/index.html"
+  },
+  {
+    "revision": "7c30685ee2d132c83739",
+    "url": "/react-components/static/js/2.ece73c23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-components/static/js/2.ece73c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "112753325d72297b864b",
+    "url": "/react-components/static/js/main.5395710b.chunk.js"
+  },
+  {
+    "revision": "be226a1e96e747e2ca07",
+    "url": "/react-components/static/js/runtime-main.73f2fc0b.js"
+  }
+]);
